f006d7c8e086681be9693891a4e5d7b14e9df525
dbarrous <damianbarrous@gmail.com>
2019-05-06 19:09:16 -0400

    Updates

---
+++ precache-manifest.ec2d8eded93c26f8c11b0336724fb8ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47f74beea640c91cc70928091d1f6e6",
+    "url": "/authorQuiz/index.html"
+  },
+  {
+    "revision": "f6920587e44e09d962c2",
+    "url": "/authorQuiz/static/css/main.e0464989.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/authorQuiz/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "f6920587e44e09d962c2",
+    "url": "/authorQuiz/static/js/main.1a631985.chunk.js"
+  },
+  {
+    "revision": "fc2bee146ed5d39f70c7",
+    "url": "/authorQuiz/static/js/runtime~main.40dce048.js"
+  }
+]);
